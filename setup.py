e19de0ee8a90ce74c70b047d8845342f83b79683
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-08-12 11:19:01 -0500

    [setup]: require pytools>=2020.4

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(name="loo.py",
           ],
 
       install_requires=[
-          "pytools>=2018.4",
+          "pytools>=2020.4",
           "pymbolic>=2019.2",
           "genpy>=2016.1.2",
           "cgen>=2016.1,<2019.1",
